1f0bb9aab1805050ec4c3d134eb2f04e2168f237
ch <ch@mail.startek-eng.com>
2019-03-25 16:07:58 +0800

    Querying documents.
    sec. 10, video 78

--- mongodb.js
+++ mongodb.js
@@ -3,61 +3,38 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-console.log(id.id.length)
-console.log(id.toHexString().length)
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
     }
 
     const db = client.db(databaseName)
-    // db.collection('users').insertOne({
-    //     name: 'CH',
-    //     age: 32
-    // }, (error, result) => {
+    
+    // db.collection('users').findOne({ _id: new ObjectID("5c987442bdec6042cc2350de") }, (error, user) => {
     //     if (error) {
-    //         return console.log('Unable to insert user')
+    //         return console.log('Unable to fetch')
     //     }
 
-    //     console.log(result.ops)
+    //     console.log(user)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jane',
-    //         age: 28
-    //     }, {
-    //         name: 'Gunther',
-    //         age: 27
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents!')
-    //     }
-
-    //     console.log(result.ops)
+    // db.collection('users').find({ age: 32 }).toArray((error, users) => {
+    //     console.log(users)
     // })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'clean the table',
-    //         completed: true 
-    //     }, {
-    //         description: 'read the book',
-    //         completed: false 
-    //     }, {
-    //         description: 'buy some food',
-    //         completed: true 
-    //     },
-    // ], (error, result) => {
+    // db.collection('tasks').findOne({ _id: new ObjectID("5c987dc416f22a3a7863f110") }, (error, task) => {
     //     if (error) {
-    //         return console.log('Unable to insert documents!')
+    //         return console.log('Unable to fetch task!')
     //     }
 
-    //     console.log(result.ops)
+    //     console.log(task)
     // })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        if (error) {
+            return console.log('Unable to fetch tasks!')
+        }
+
+        console.log(tasks)
+    })
 })
