7aec6feb416744d3e4e1744a3841a923ab1400f4
ch <ch@mail.startek-eng.com>
2019-03-25 15:11:03 +0800

    Inserting documents.
    sec. 10, video 76

--- mongodb.js
+++ mongodb.js
@@ -10,8 +10,49 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'CH',
-        age: 32
+    // db.collection('users').insertOne({
+    //     name: 'CH',
+    //     age: 32
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jane',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'clean the table',
+            completed: true 
+        }, {
+            description: 'read the book',
+            completed: false 
+        }, {
+            description: 'buy some food',
+            completed: true 
+        },
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.ops)
     })
 })
