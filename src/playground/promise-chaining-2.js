34f673c9735dac356dca0bed9e7fff3086876dc0
ch <ch@mail.startek-eng.com>
2019-03-27 17:34:09 +0800

    Async/Await: part II
    sec. 11, video 96

--- src/playground/promise-chaining-2.js
+++ src/playground/promise-chaining-2.js
@@ -1,10 +1,22 @@
 require('../db/mongoose')
 const Task = require('../models/task')
 
-Task.findByIdAndDelete('5c9ad26c9ec16b4ccca00f6a').then(() => {
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5c9ad26c9ec16b4ccca00f6a').then(() => {
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndDelete(id)
+    const count = Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5c99d08c2271434878f84bfc').then((count) => {
+    console.log('count:', count)
 }).catch((e) => {
     console.log(e)
 })
--- src/playground/promise-chaining.js
+++ src/playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../db/mongoose')
 const User = require('../models/user')
 
-User.findByIdAndUpdate('5c99f32fc58a33103459bd50', { age: 1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5c99f32fc58a33103459bd50', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5c99f32fc58a33103459bd50', 2).then((count) => {
+    console.log('count:', count)
 }).catch((e) => {
-    console.log(e)
+    console.log('e:', e)
 })
