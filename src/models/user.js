160f42eb12bb6f5d94a96277f10cecd84fd089cc
ch <ch@mail.startek-eng.com>
2019-03-29 10:40:47 +0800

    Generating authentication tokens
    sec. 12, video 107
    
    Static methods (Model methods) are accessible on the Model.
    Instance methods are accessible on the instances.
    
    https://mongoosejs.com/docs/guide.html#methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,9 +40,27 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+// Instance methods
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisisfromCH')
+    
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+// Statics
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
