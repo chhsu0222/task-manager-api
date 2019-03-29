4862802c8222f42f3fbdc8a734d3b3a8a00da8bf
ch <ch@mail.startek-eng.com>
2019-03-29 14:45:05 +0800

    Accepting authentication tokens
    sec. 12, video 109
    
    Applying auth middleware before route /users/me

--- src/index.js
+++ src/index.js
@@ -6,17 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         return res.status(400).send('GET requests are disabled')
-//     }
-//     next()
-// })
-
-app.use((req, res, next) => {
-    res.status(503).send('Website under maintenance...')
-})
-
 app.use(express.json()) // parse incoming json to an object (req.body)
 app.use(userRouter)
 app.use(taskRouter)
---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+// auth will find a user with the correct ID who has that
+// authentication token still stored.
+// The header in the request is Authorization with value 'Bearer [token]'
+// If the user logs out, that means this token will be invalid.
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisisfromCH')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if (!user) {
+            throw new Error()
+        }
+
+        req.user = user // put the user info into req for the following route handler
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
+}
+
+module.exports = auth
