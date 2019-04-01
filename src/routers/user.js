75f2ff9a6ce999f541a007c2f8fe42a4e13ac99f
ch <ch@mail.startek-eng.com>
2019-04-01 10:49:29 +0800

    Authenticating User endpoints
    sec. 12, video 113
    
    https://github.com/Automattic/mongoose/issues/6295
    
    In Mongoose, a "document" generally means an instance of a model.
    https://mongoosejs.com/docs/documents.html
    
    Model.prototype.remove()
    https://mongoosejs.com/docs/api.html#model_Model-remove

--- src/routers/user.js
+++ src/routers/user.js
@@ -55,22 +55,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {  // route parameters
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -80,28 +65,20 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        // When you call .remove() on a document,
+        // you're actually calling Model.prototype.remove()
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
