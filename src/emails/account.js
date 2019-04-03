e5fc1060dc5d0197f08124f506cf19db9dfc66d0
ch <ch@mail.startek-eng.com>
2019-04-03 13:59:58 +0800

    Sending welcome and cancelation emails
    sec. 15, video 132

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,25 @@ const sendgridAPIKey = 'SG._6QS711yT-is593KPMDS0Q.3ryOfOun2useHty0RcBVWxGJu8N3UN
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to: 'chhsu0222@gmail.com',
-    from: 'chhsu0222@gmail.com',
-    subject: 'This is my first creation',
-    text: 'I hope this one actually get to you.'
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'chhsu0222@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'chhsu0222@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
