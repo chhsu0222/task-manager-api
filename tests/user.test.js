e722b53d50a614f29ba2d6b4427f05b027af6892
ch <ch@mail.startek-eng.com>
2019-04-09 09:40:49 +0800

    Advanced assertions
    sec. 16, video 145
    
    https://jestjs.io/docs/en/23.x/expect
    https://jestjs.io/docs/en/23.x/expect#tomatchobjectobject

--- tests/user.test.js
+++ tests/user.test.js
@@ -21,18 +21,37 @@ beforeEach(async () => {
 })
 
 test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'CH Hsu',
         email: 'ch@example.com',
         password: 'MyPass123!'
     }).expect(201)
+
+    // Assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'CH Hsu',
+            email: 'ch@example.com'
+        },
+        token: user.tokens[0].token
+    })
+
+    // Assert thatthe plain text password is not stored in the database
+    expect(user.password).not.toBe('MyPass123!')
 })
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login noexistent user', async () => {
@@ -63,6 +82,9 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+    
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Should not delete account for unauthenticated user', async () => {
