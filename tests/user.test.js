44d1860826bd97cc2452860f7855959bce23287a
ch <ch@mail.startek-eng.com>
2019-04-09 14:42:13 +0800

    Wrapping up user tests
    sec. 16, video 147
    
    Using 'attach' to upload multipart files (supertest)

--- tests/user.test.js
+++ tests/user.test.js
@@ -93,3 +93,33 @@ test('Should not delete account for unauthenticate user', async () => {
         .send()
         .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    // check if user.avatar equals any buffer
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({ name: 'John' })
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.name).toBe('John')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({ location: 'Taipei' })
+        .expect(400)
+})
